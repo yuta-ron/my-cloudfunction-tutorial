821006d7851eedc7478969e8b6ec622c51641234
yutaroon <y-nishi@adventure-inc.co.jp>
2021-06-02 11:47:01 +0900

    [fix] pubsub-function

--- pubsub-func/function.go
+++ pubsub-func/function.go
@@ -6,7 +6,7 @@ import (
 	"log"
 )
 
-// PubSubMessage is the payload of a Pub/Sub event. Please refer to the docs for
+// MyScheduledFunction is the payload of a Pub/Sub event. Please refer to the docs for
 // additional information regarding Pub/Sub events.
 type PubSubMessage struct {
 	Data []byte `json:"data"`
